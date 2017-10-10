95e8837b944988328c3d11d02382f8ab86df49b4
alvasenj <alvasenj@ucm.es>
2017-10-10 23:34:11 +0200

    movido a carpetas

--- doc/db_propuesta.sql
+++ doc/db_propuesta.sql
@@ -1,77 +1,120 @@
 CREATE TABLE `Usuarios` (
-	`nick` VARCHAR(20) NOT NULL AUTO_INCREMENT,
-	`Nombre` VARCHAR(30) NOT NULL AUTO_INCREMENT,
-	`Apellidos` VARCHAR(60) NOT NULL AUTO_INCREMENT,
-	`rol` VARCHAR(20) NOT NULL AUTO_INCREMENT,
+	`nick` varchar(20) NOT NULL,
+	`Nombre` varchar(30) NOT NULL,
+	`Apellidos` varchar(60) NOT NULL,
+	`rol` varchar(20) NOT NULL,
+	`nivel` INT NOT NULL,
 	PRIMARY KEY (`nick`)
 );
 
 CREATE TABLE `Insignias` (
-	`nivel` INT NOT NULL AUTO_INCREMENT,
-	`Descripción` TEXT(100) NOT NULL AUTO_INCREMENT,
-	`Foto` BINARY(100) NOT NULL AUTO_INCREMENT,
-	`user` VARCHAR(20) NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (`nivel`)
+	`nivel` int NOT NULL,
+	`Descripción` TEXT(100) NOT NULL,
+	`rol` varchar(20) NOT NULL,
+	PRIMARY KEY (`nivel`,`rol`)
 );
 
 CREATE TABLE `Modificaciones` (
-	`id_modificacion` INT NOT NULL AUTO_INCREMENT,
-	`votos_positivos` INT NOT NULL AUTO_INCREMENT DEFAULT '0',
-	`votos_negativos` INT NOT NULL AUTO_INCREMENT DEFAULT '0',
-	`id_user` VARCHAR(20) NOT NULL AUTO_INCREMENT,
+	`id_modificacion` varchar NOT NULL,
+	`votos_positivos` int NOT NULL DEFAULT '0',
+	`votos_negativos` int NOT NULL DEFAULT '0',
+	`id_user` varchar(20) NOT NULL,
 	PRIMARY KEY (`id_modificacion`)
 );
 
 CREATE TABLE `Exámenes` (
-	`Fecha_inicio` DATE NOT NULL AUTO_INCREMENT,
-	`Fecha_fin` DATE NOT NULL AUTO_INCREMENT,
+	`id_ex` INT NOT NULL AUTO_INCREMENT,
 	`nivel` INT NOT NULL,
-	PRIMARY KEY (`Fecha_inicio`,`Fecha_fin`)
+	`rol` varchar(20) NOT NULL,
+	PRIMARY KEY (`id_ex`,`nivel`)
 );
 
 CREATE TABLE `Realiza_Examen` (
-	`nick_usr` VARCHAR(20) NOT NULL AUTO_INCREMENT,
-	`Ex_inicio` DATE NOT NULL AUTO_INCREMENT,
-	`Ex_fin` DATE NOT NULL AUTO_INCREMENT,
-	`nota` INT NOT NULL DEFAULT '0',
-	PRIMARY KEY (`nick_usr`,`Ex_inicio`,`Ex_fin`)
+	`nick_usr` varchar(20) NOT NULL,
+	`id_examen` DATE NOT NULL AUTO_INCREMENT,
+	`nota` int NOT NULL DEFAULT '0',
+	PRIMARY KEY (`nick_usr`,`id_examen`)
 );
 
 CREATE TABLE `Preguntas` (
-	`id_modificación` varchar NOT NULL AUTO_INCREMENT,
+	`id_pregunta` varchar NOT NULL,
 	`nivel` INT NOT NULL,
 	`texto` TEXT(200) NOT NULL,
-	`puntuación` INT NOT NULL,
-	PRIMARY KEY (`id_modificación`)
+	`rol` varchar(20) NOT NULL,
+	`A` TEXT NOT NULL,
+	`B` TEXT NOT NULL,
+	`C` TEXT NOT NULL,
+	`D` TEXT NOT NULL,
+	`Correcta` TEXT NOT NULL,
+	PRIMARY KEY (`id_pregunta`,`nivel`)
 );
 
 CREATE TABLE `Preguntas_Examen` (
-	`Examen_ini` DATE NOT NULL AUTO_INCREMENT,
-	`Examen_fin` DATE NOT NULL AUTO_INCREMENT,
-	`pregunta_id` varchar NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (`Examen_ini`,`Examen_fin`,`pregunta_id`)
+	`id_exam` INT NOT NULL,
+	`pregunta_id` varchar NOT NULL,
+	PRIMARY KEY (`id_exam`,`pregunta_id`)
 );
 
 CREATE TABLE `Artículos` (
-	`Nivel_articulo` INT NOT NULL AUTO_INCREMENT,
-	`id_articulo` INT NOT NULL AUTO_INCREMENT,
-	`Nombre_capitulo` VARCHAR(30) NOT NULL,
-	PRIMARY KEY (`Nivel_articulo`,`id_articulo`)
+	`Nivel_articulo` int NOT NULL,
+	`id_articulo` FLOAT NOT NULL AUTO_INCREMENT,
+	`Nombre_articulo` varchar(30) NOT NULL,
+	`rol_articulo` varchar(20) NOT NULL,
+	PRIMARY KEY (`Nivel_articulo`,`id_articulo`,`rol_articulo`)
 );
 
-ALTER TABLE `Insignias` ADD CONSTRAINT `Insignias_fk0` FOREIGN KEY (`user`) REFERENCES `Usuarios`(`nick`);
+CREATE TABLE `Insignias_Usuario` (
+	`usr_nickname` varchar(20) NOT NULL,
+	`nivel_insignia` INT NOT NULL,
+	`rol_insignia` varchar(20) NOT NULL,
+	PRIMARY KEY (`usr_nickname`,`nivel_insignia`,`rol_insignia`)
+);
+
+CREATE TABLE `Modificacion_pregunta` (
+	`rol` varchar(20) NOT NULL,
+	`id_mod` varchar NOT NULL,
+	`nivel` INT NOT NULL,
+	`texto` TEXT(200) NOT NULL,
+	`A` TEXT NOT NULL,
+	`B` TEXT NOT NULL,
+	`C` TEXT NOT NULL,
+	`D` TEXT NOT NULL,
+	`Correcta` TEXT NOT NULL,
+	PRIMARY KEY (`id_mod`)
+);
+
+CREATE TABLE `Modificacion_sugerencia` (
+	`id_mod` varchar NOT NULL,
+	`texto` TEXT NOT NULL,
+	PRIMARY KEY (`id_mod`)
+);
+
+CREATE TABLE `Modificacion_contenido` (
+	`id_mod` varchar NOT NULL,
+	`rol` varchar(20) NOT NULL,
+	`nivel` INT(20) NOT NULL,
+	PRIMARY KEY (`id_mod`)
+);
 
 ALTER TABLE `Modificaciones` ADD CONSTRAINT `Modificaciones_fk0` FOREIGN KEY (`id_user`) REFERENCES `Usuarios`(`nick`);
 
 ALTER TABLE `Realiza_Examen` ADD CONSTRAINT `Realiza_Examen_fk0` FOREIGN KEY (`nick_usr`) REFERENCES `Usuarios`(`nick`);
 
-ALTER TABLE `Realiza_Examen` ADD CONSTRAINT `Realiza_Examen_fk1` FOREIGN KEY (`Ex_inicio`) REFERENCES `Exámenes`(`Fecha_inicio`);
+ALTER TABLE `Realiza_Examen` ADD CONSTRAINT `Realiza_Examen_fk1` FOREIGN KEY (`id_examen`) REFERENCES `Exámenes`(`id_ex`);
 
-ALTER TABLE `Realiza_Examen` ADD CONSTRAINT `Realiza_Examen_fk2` FOREIGN KEY (`Ex_fin`) REFERENCES `Exámenes`(`Fecha_fin`);
+ALTER TABLE `Preguntas_Examen` ADD CONSTRAINT `Preguntas_Examen_fk0` FOREIGN KEY (`id_exam`) REFERENCES `Exámenes`(`id_ex`);
 
-ALTER TABLE `Preguntas_Examen` ADD CONSTRAINT `Preguntas_Examen_fk0` FOREIGN KEY (`Examen_ini`) REFERENCES `Exámenes`(`Fecha_inicio`);
+ALTER TABLE `Preguntas_Examen` ADD CONSTRAINT `Preguntas_Examen_fk1` FOREIGN KEY (`pregunta_id`) REFERENCES `Preguntas`(`id_pregunta`);
 
-ALTER TABLE `Preguntas_Examen` ADD CONSTRAINT `Preguntas_Examen_fk1` FOREIGN KEY (`Examen_fin`) REFERENCES `Exámenes`(`Fecha_fin`);
+ALTER TABLE `Insignias_Usuario` ADD CONSTRAINT `Insignias_Usuario_fk0` FOREIGN KEY (`usr_nickname`) REFERENCES `Usuarios`(`nick`);
 
-ALTER TABLE `Preguntas_Examen` ADD CONSTRAINT `Preguntas_Examen_fk2` FOREIGN KEY (`pregunta_id`) REFERENCES `Preguntas`(`id_modificación`);
+ALTER TABLE `Insignias_Usuario` ADD CONSTRAINT `Insignias_Usuario_fk1` FOREIGN KEY (`nivel_insignia`) REFERENCES `Insignias`(`nivel`);
+
+ALTER TABLE `Insignias_Usuario` ADD CONSTRAINT `Insignias_Usuario_fk2` FOREIGN KEY (`rol_insignia`) REFERENCES `Insignias`(`rol`);
+
+ALTER TABLE `Modificacion_pregunta` ADD CONSTRAINT `Modificacion_pregunta_fk0` FOREIGN KEY (`id_mod`) REFERENCES `Modificaciones`(`id_modificacion`);
+
+ALTER TABLE `Modificacion_sugerencia` ADD CONSTRAINT `Modificacion_sugerencia_fk0` FOREIGN KEY (`id_mod`) REFERENCES `Modificaciones`(`id_modificacion`);
+
+ALTER TABLE `Modificacion_contenido` ADD CONSTRAINT `Modificacion_contenido_fk0` FOREIGN KEY (`id_mod`) REFERENCES `Modificaciones`(`id_modificacion`);
 
