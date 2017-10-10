564230a51d71288aeca851f3514ad81f88ee4bde
javimv36 <javimv36@gmail.com>
2017-10-10 20:47:57 +0200

    Gitignore de la conexion y prueba

--- backend/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $con = @mysqli_connect('localhost', '', '', '');
-
-    if (!$con) {
-        echo "Error: " . mysqli_connect_error();
-        exit();
-    }
-    echo 'Conectado a la base de datos MySQL';
-?>
