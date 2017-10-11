61f565afc565597c9e44e4b32cbec1c71d781667
javimv36 <javimv36@gmail.com>
2017-10-11 10:35:00 +0200

    Añadido favicon, estructura CSS y archivos (imagenes, fuentes...)

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <html>
   <head>
     <title>Kapi - Home</title>
+    <link rel="icon" href="files/icon/logo.png" type="image/gif" sizes="16x16">
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <meta charset="utf-8">
   </head>
   <body>
       <h1>Kapi solutions Home</h1>
@@ -9,7 +12,6 @@
     <p>Conexion a la base de datos:
     <?php 
      include 'backend/conexion.php';
-
      include 'backend/desconexion.php';
     ?>
   </body>
