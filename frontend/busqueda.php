c9d5176a24f722db3a4cc17d4f5878b0266f03bc
javimv36 <javimv36@gmail.com>
2017-10-27 17:24:14 +0200

    Intentando añadir icono svg para manipular botones

--- frontend/busqueda.php
+++ frontend/busqueda.php
@@ -1,6 +1,20 @@
 <?php
      require 'backend/busqueda.php';
 ?>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: none;">
+<defs>
+<g id="icon-image">
+	<path class="path1" d="M227.002,108.256c-2.755-41.751-37.6-74.878-80.036-74.878c-42.447,0-77.298,33.141-80.038,74.907
+		C28.978,113.059,0,145.39,0,184.184c0,42.234,34.36,76.595,76.595,76.595h116.483c3.313,0,6-2.687,6-6s-2.687-6-6-6H76.595
+		C40.977,248.778,12,219.801,12,184.184c0-34.275,26.833-62.568,61.087-64.411c3.184-0.171,5.678-2.803,5.678-5.991
+		c0-0.119-0.003-0.236-0.01-0.355c0.09-37.536,30.654-68.049,68.211-68.049c37.563,0,68.132,30.518,68.211,68.063
+		c-0.005,0.116-0.009,0.238-0.009,0.329c0,3.196,2.505,5.831,5.696,5.992c34.37,1.741,61.292,30.038,61.292,64.421
+		c0,19.526-8.698,37.801-23.864,50.138c-2.571,2.091-2.959,5.87-0.868,8.44c2.091,2.571,5.87,2.959,8.44,0.868
+		c17.98-14.626,28.292-36.293,28.292-59.447C294.156,145.269,265.08,112.926,227.002,108.256z"></path>
+</g>
+</defs>
+</svg>
+
 <div id="content">
     <h3> Resultados de: <?php echo $_GET["search"] ?> </h3>
 
@@ -8,7 +22,7 @@
     <div class="articulos">
     <?php
         for ($i = 0; $i < sizeof($busquedaArticulos) ; $i++){
-            echo '<div class="articulo primario-light">';
+            echo '<div class="articulo">';
                 echo '<a href="/articulo.php?id='.$busquedaArticulos[$i]['Nivel_articulo'].'">';
                     echo '<div class="infoArticulo">';
                         echo  '<p> ' . $busquedaArticulos[$i]['Nivel_articulo']. '</p>';
@@ -16,6 +30,12 @@
                         echo  '<p> ' . $busquedaArticulos[$i]['id_creador']. '</p>';
                         echo  '<p> ' . $busquedaArticulos[$i]['Imagen'] . '</p>';
                         echo  '<p> ' . $busquedaArticulos[$i]['rol_articulo'] . '</p>';
+                      //  echo  '<div class="botonVer"></div>';
+                        ?>
+                             <svg viewBox="0 0 32 32">
+                             <use xlink:href="#icon-image"></use>
+                           </svg>
+                        <?php
                     echo '</div>';
                     echo '<div class="contenidoArticulo">';
                         echo  '<p> ' . $busquedaArticulos[$i]['titulo']. '</p>';
