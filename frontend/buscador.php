cd4af78d671ae29c8183ed37596650c19680f322
javimv36 <javimv36@gmail.com>
2017-10-29 02:29:06 +0200

    Placeholder buscador indica busqueda
    En la barra de busqueda de la web, aparece lo
    buscado si se ha buscado algo y escribe
    Busqueda... si no se ha buscado nada

--- frontend/buscador.php
+++ frontend/buscador.php
@@ -1,4 +1,13 @@
 <form class="form-wrapper cf" action="busqueda.php">
-		<input name="search" type="text" placeholder="Busqueda..." >
+		<input name="search" type="text" placeholder=
+			<?php 
+		
+				if(isset($_GET["search"])&&$_GET["search"]!=""){
+						echo '"'.$_GET["search"].'"';
+					}else{
+						echo '"'.'Busqueda...'.'"';
+					}
+			?>
+		>
 	  <button type="submit">Buscar</button>
 </form>
