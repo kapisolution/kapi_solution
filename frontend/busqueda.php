d29a7ab8a59889b808b757bb3ed311299b2668eb
javimv36 <javimv36@gmail.com>
2017-10-29 20:01:46 +0100

    Añadiendo estilo usuarios
    Los usuarios se muestran antes que los articulos
    y con la imagen de usuario a la izquierda

--- frontend/busqueda.php
+++ frontend/busqueda.php
@@ -4,16 +4,61 @@
 <div id="content">
     <h3> Resultados de: <?php echo $_GET["search"] ?> </h3>
 
+     
+    <h4>Usuarios:</h4>
+    <div class="tarjetas">
+    <?php
+        for ($i = 0; $i < sizeof($busquedaUsuarios) ; $i++){
+            echo '<div class="tarjeta">';
+            
+                  echo '<div class="infoTarjeta" style="background-image: url(&quot;/files/img/rol/' . $busquedaUsuarios[$i]['rol']. '.jpg&quot;);">';
+                  echo '<a class="sinSub" href="/usuario.php?id='.$busquedaUsuarios[$i]['nick'].'">';
+                   ?>
+                   <img class="iconoTarjeta imgUser" src="/files/img/usuario/<?php echo $busquedaUsuarios[$i]['nick'].'.jpg';?>">
+                    
+
+                     <?php
+                     echo '</a>'; 
+                     echo '<p>' . $busquedaUsuarios[$i]['nick'] . '</p>';
+                     echo '<a class="sinSub" href="/usuario.php?id='.$busquedaUsuarios[$i]['nick'].'">';
+                     ?>
+                     
+                    <svg class="SVGsecundario iconoTarjeta" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    viewBox="0 0 294.843 294.843" style="enable-background:new 0 0 294.843 294.843;" xml:space="preserve">
+               <g>
+                   <path d="M278.527,79.946c-10.324-20.023-25.38-37.704-43.538-51.132c-2.665-1.97-6.421-1.407-8.392,1.257s-1.407,6.421,1.257,8.392
+                       c16.687,12.34,30.521,28.586,40.008,46.983c9.94,19.277,14.98,40.128,14.98,61.976c0,74.671-60.75,135.421-135.421,135.421
+                       S12,222.093,12,147.421S72.75,12,147.421,12c3.313,0,6-2.687,6-6s-2.687-6-6-6C66.133,0,0,66.133,0,147.421
+                       s66.133,147.421,147.421,147.421s147.421-66.133,147.421-147.421C294.842,123.977,289.201,100.645,278.527,79.946z"/>
+                   <path d="M109.699,78.969c-1.876,1.067-3.035,3.059-3.035,5.216v131.674c0,3.314,2.687,6,6,6s6-2.686,6-6V94.74l88.833,52.883
+                       l-65.324,42.087c-2.785,1.795-3.589,5.508-1.794,8.293c1.796,2.786,5.508,3.59,8.294,1.794l73.465-47.333
+                       c1.746-1.125,2.786-3.073,2.749-5.15c-0.037-2.077-1.145-3.987-2.93-5.05L115.733,79.029
+                       C113.877,77.926,111.575,77.902,109.699,78.969z"/>
+               </g>
+               </svg>
+                      <?php
+                      echo '</a>';
+                  echo '</div>';
+                  echo '<div class="contenidoTarjeta">';
+                  echo '<p>' . $busquedaUsuarios[$i]['Nombre'] . '</p>';
+                  echo '<p>' . $busquedaUsuarios[$i]['Apellidos'] . '</p>';
+                echo '</div>';
+              
+          echo '</div>';
+        }
+    ?>
+    </div>
+
     <h4>Articulos:</h4>
-    <div class="articulos">
+    <div class="tarjetas">
     <?php
         for ($i = 0; $i < sizeof($busquedaArticulos) ; $i++){
-            echo '<div class="articulo">';
+            echo '<div class="tarjeta">';
               
-                    echo '<div class="infoArticulo" style="background-image: url(&quot;/files/img/articulos/' . $busquedaArticulos[$i]['capitulo']. '.jpg&quot;);">';
+                    echo '<div class="infoTarjeta" style="background-image: url(&quot;/files/img/articulos/' . $busquedaArticulos[$i]['capitulo']. '.jpg&quot;);">';
                     echo '<a class="sinSub" href="/articulo.php?id='.$busquedaArticulos[$i]['Nivel_articulo'].'">';
                      ?>
-                      <svg class="SVGsecundario iconoArticulo" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      <svg class="SVGsecundario iconoTarjeta" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                             viewBox="0 0 294.774 294.774" style="enable-background:new 0 0 294.774 294.774;" xml:space="preserve">
                         <g>
                             <path d="M251.63,43.179c-11.295-11.295-23.931-20.472-37.556-27.276c-2.966-1.482-6.568-0.277-8.048,2.687
@@ -37,7 +82,7 @@
                         echo '<a class="sinSub" href="/articulo.php?id='.$busquedaArticulos[$i]['Nivel_articulo'].'">';
                        ?>
                       
-                      <svg class="SVGsecundario iconoArticulo" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      <svg class="SVGsecundario iconoTarjeta" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                       viewBox="0 0 294.843 294.843" style="enable-background:new 0 0 294.843 294.843;" xml:space="preserve">
                  <g>
                      <path d="M278.527,79.946c-10.324-20.023-25.38-37.704-43.538-51.132c-2.665-1.97-6.421-1.407-8.392,1.257s-1.407,6.421,1.257,8.392
@@ -53,7 +98,7 @@
                         <?php
                         echo '</a>';
                     echo '</div>';
-                    echo '<div class="contenidoArticulo">';
+                    echo '<div class="contenidoTarjeta">';
                         echo  '<p> ' . $busquedaArticulos[$i]['titulo']. '</p>';
                         echo  '<p> ' . $busquedaArticulos[$i]['id_creador']. '</p>';
                         echo  '<p> ' . $busquedaArticulos[$i]['rol_articulo'] . '</p>';
@@ -64,22 +109,10 @@
         }
     ?>
     </div>
+
     <?php
         if (isset($_SESSION["login"])){
     ?>
-    <h4>Usuarios:</h4>
-    <div class="usuarios">
-    <?php
-        for ($i = 0; $i < sizeof($busquedaUsuarios) ; $i++){
-            echo '<div class="usuario">';
-                echo '<p>' . $busquedaUsuarios[$i]['nick'] . '</p>';
-                echo '<p>' . $busquedaUsuarios[$i]['nombre'] . '</p>';
-                echo '<p>' . $busquedaUsuarios[$i]['apellidos'] . '</p>';
-            echo '</div>';
-        }
-    ?>
-    </div>
-
     <h4>Ejercicios:</h4>
     <div class="ejercicios">
     <?php
