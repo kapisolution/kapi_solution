77a35121d9e0b27fe209334c4a0c5a59528c1895
javimv36 <javimv36@gmail.com>
2017-10-31 23:39:02 +0100

    Barra de articulos relacionados
    A la derecha del articulo que se muestra, se
    muestra una barra (ahora vacia) en la que se
    añadiran los articulos relacionados con el
    mostrado.
    
    Los anchos se adaptan con tamaños minimos.

--- frontend/articulo.php
+++ frontend/articulo.php
@@ -2,6 +2,8 @@
      require 'backend/articulo.php';
 ?>
 <div id="content">
+<div id="bloque">    
+<div id="articulo">
     <?php
         for ($i = 0; $i < sizeof($busquedaArticulos) ; $i++){
             ?>
@@ -10,22 +12,32 @@
             echo  '<h1> Lvl:' . $busquedaArticulos[$i]['Nivel_articulo']. '</h1>';
             echo  '<h1> Ch: ' . $busquedaArticulos[$i]['capitulo']. '</h1>';
             ?>
+            <div>
             <img class="imgInfoArticulo" src="/files/img/usuario/<?php echo $busquedaArticulos[$i]['id_creador'].'.jpg';?>">
             <?php
             echo  '<p>' . $busquedaArticulos[$i]['id_creador']. '</p>';
             ?>
+            </div>
+            <div>
             <img class="imgInfoArticulo" src="/files/img/rol/<?php echo $busquedaArticulos[$i]['rol_articulo']. '.jpg'?>">
-           <?php
+            <?php
             echo  '<p>' . $busquedaArticulos[$i]['rol_articulo'] . '</p>';
             ?>
+            </div>
             </div>
             <?php
             echo  '<h2> ' . $busquedaArticulos[$i]['titulo']. '</h1>';
             ?>
             <img class="imgArticulo" src="/files/img/articulos/<?php echo $busquedaArticulos[$i]['Imagen']. '.jpg'?>">
-           <?php
+            <?php
             echo  '<p> Img: ' . $busquedaArticulos[$i]['Imagen'] . '.jpg</p>';
             echo  '<p> ' . $busquedaArticulos[$i]['contenido'] . '</p>';
         }
-    ?>
+
+?>
+</div>
+<div id="articulosRelacionados">
+    <p>Articulos relacionados</p>
+    </div>
+</div>
 </div>
