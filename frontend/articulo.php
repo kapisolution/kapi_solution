deda2385255105775abb82fd14bfefb5219e4f22
javimv36 <javimv36@gmail.com>
2017-10-31 16:51:36 +0100

    Colocando imagenes e informacion basica del articulo
    Coloco en la parte superior con flex los
    atriburos de informacion del articulo, debajo y
    ocupando todo el ancho de la pagina, muestro el
    titulo, la imagen asociada al articulo y el
    contenido del mismo.

--- frontend/articulo.php
+++ frontend/articulo.php
@@ -4,12 +4,27 @@
 <div id="content">
     <?php
         for ($i = 0; $i < sizeof($busquedaArticulos) ; $i++){
+            ?>
+            <div id="infoArticulo">
+            <?php
             echo  '<h1> Lvl:' . $busquedaArticulos[$i]['Nivel_articulo']. '</h1>';
             echo  '<h1> Ch: ' . $busquedaArticulos[$i]['capitulo']. '</h1>';
-            echo  '<p> Auth: ' . $busquedaArticulos[$i]['id_creador']. '</p>';
-            echo  '<p> Rol: ' . $busquedaArticulos[$i]['rol_articulo'] . '</p>';
-            echo  '<p> Img: ' . $busquedaArticulos[$i]['Imagen'] . '</p>';
+            ?>
+            <img class="imgInfoArticulo" src="/files/img/usuario/<?php echo $busquedaArticulos[$i]['id_creador'].'.jpg';?>">
+            <?php
+            echo  '<p>' . $busquedaArticulos[$i]['id_creador']. '</p>';
+            ?>
+            <img class="imgInfoArticulo" src="/files/img/rol/<?php echo $busquedaArticulos[$i]['rol_articulo']. '.jpg'?>">
+           <?php
+            echo  '<p>' . $busquedaArticulos[$i]['rol_articulo'] . '</p>';
+            ?>
+            </div>
+            <?php
             echo  '<h2> ' . $busquedaArticulos[$i]['titulo']. '</h1>';
+            ?>
+            <img class="imgArticulo" src="/files/img/articulos/<?php echo $busquedaArticulos[$i]['Imagen']. '.jpg'?>">
+           <?php
+            echo  '<p> Img: ' . $busquedaArticulos[$i]['Imagen'] . '.jpg</p>';
             echo  '<p> ' . $busquedaArticulos[$i]['contenido'] . '</p>';
         }
     ?>
