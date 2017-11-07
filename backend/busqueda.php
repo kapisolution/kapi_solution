6a23435c35a815e592d5982f4e6c38a81acb7307
Javier Martín <javmar12@ucm.es>
2017-11-07 11:39:02 +0100

    Adaptando consulta a nuevas columnas de usuario en la BBDD

--- backend/busqueda.php
+++ backend/busqueda.php
@@ -7,7 +7,7 @@ if(isset($_GET["search"])&&$_GET["search"]!=""){
         $busquedaArticulos[] = $fila;
     }
 
-    $sql ='SELECT * FROM Usuarios WHERE nick LIKE "%'.$_GET["search"] .'%" OR nombre LIKE "%'.$_GET["search"] .'%" OR apellidos LIKE "%'.$_GET["search"] .'%";';
+    $sql ='SELECT * FROM Usuarios WHERE nick LIKE "%'.$_GET["search"] .'%";';
     $consulta = mysqli_query($con, $sql) or die("Error en consulta sobre la tabla usuarios");
     $busquedaUsuarios = array();
     while($fila = mysqli_fetch_array($consulta)){
