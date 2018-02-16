65b560d6669e6d4a79b33178c3fe862a12452820
alvasenj <alvasenj@ucm.es>
2018-02-16 20:41:25 +0100

    comenzando a integrar BS en el buscador

--- busqueda.php
+++ busqueda.php
@@ -1,21 +1,28 @@
 <html>
   <head>
+  <!--Cabeceras bootstrap -->
+  <script src="js/tabs.js"></script> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!--Cabeceras bootstrap -->
+    <link rel="stylesheet" type="text/css" href="css/topbar.css" />
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+    <link rel="stylesheet" type="text/css" href="css/busqueda.css" />
+    <link rel="stylesheet" type="text/css" href="css/estilos.css" />
+    
     <title>Kapi - Búsqueda</title>
     <link rel="icon" href="files/icon/logo.png" type="image/gif" sizes="16x16">
-    <link rel="stylesheet" type="text/css" href="css/index.css" />
-    <link rel="stylesheet" type="text/css" href="css/topbar.css" />
-    <link rel="stylesheet" type="text/css" href="css/busqueda.css" />
     <meta charset="utf-8">
   </head>
   <body>
     <?php 
      require 'backend/conexion.php';
      include 'frontend/topbar.php';
-     include 'frontend/leftbar.php';
      include 'frontend/busqueda.php';
      include 'frontend/footer.php';
      include 'backend/desconexion.php';
     ?>
-    <script src="js/tabs.js"></script> 
   </body>
 </html>
--- frontend/busqueda.php
+++ frontend/busqueda.php
@@ -1,115 +1,38 @@
 <?php
      require 'backend/busqueda.php';
 ?>
-<div id="content">
+<div class="content">
+<div class="album py-5 bg-light">
     <h3> Resultados de: <?php echo $_GET["search"] ?> </h3>
-    
-    <div class="tabs">
-        <button class="tablink tab-selected" onclick="mostrarTarjetas(event, 'articulos')">Artículos</button>
-        <button class="tablink" onclick="mostrarTarjetas(event, 'usuarios')">Usuarios</button>
-        <button class="tablink" onclick="mostrarTarjetas(event, 'ejercicios')">Ejercicios</button>
-    </div> 
-
-    <div class="tarjetas" id="usuarios" style="display:none">
+    <div class="container">
+         <div class="row">
     <?php
         for ($i = 0; $i < sizeof($busquedaUsuarios) ; $i++){
-            echo '<div class="tarjeta">';            
-                echo '<div class="infoTarjeta" style="background-image: url(&quot;/files/img/rol/' . $busquedaUsuarios[$i]['rol']. '.jpg&quot;);">';
-                echo '<a class="sinSub" href="/usuario.php?id='.$busquedaUsuarios[$i]['nick'].'">';
-                    ?>
-                    <img class="imgUser" src="/files/img/usuario/<?php echo $busquedaUsuarios[$i]['nick'].'.jpg';?>">
-                    <?php
-                echo '</a>'; 
-              
-                echo '<a class="sinSub" href="/usuario.php?id='.$busquedaUsuarios[$i]['nick'].'">';
-                    ?> 
-                    <svg class="SVGsecundario iconoTarjeta" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 294.843 294.843" style="enable-background:new 0 0 294.843 294.843;" xml:space="preserve">
-                        <g>
-                            <path d="M278.527,79.946c-10.324-20.023-25.38-37.704-43.538-51.132c-2.665-1.97-6.421-1.407-8.392,1.257s-1.407,6.421,1.257,8.392
-                                c16.687,12.34,30.521,28.586,40.008,46.983c9.94,19.277,14.98,40.128,14.98,61.976c0,74.671-60.75,135.421-135.421,135.421
-                                S12,222.093,12,147.421S72.75,12,147.421,12c3.313,0,6-2.687,6-6s-2.687-6-6-6C66.133,0,0,66.133,0,147.421
-                                s66.133,147.421,147.421,147.421s147.421-66.133,147.421-147.421C294.842,123.977,289.201,100.645,278.527,79.946z"/>
-                            <path d="M109.699,78.969c-1.876,1.067-3.035,3.059-3.035,5.216v131.674c0,3.314,2.687,6,6,6s6-2.686,6-6V94.74l88.833,52.883
-                                l-65.324,42.087c-2.785,1.795-3.589,5.508-1.794,8.293c1.796,2.786,5.508,3.59,8.294,1.794l73.465-47.333
-                                c1.746-1.125,2.786-3.073,2.749-5.15c-0.037-2.077-1.145-3.987-2.93-5.05L115.733,79.029
-                                C113.877,77.926,111.575,77.902,109.699,78.969z"/>
-                        </g>
-                    </svg>
-                    <?php
-                echo '</a>';
-            echo '</div>';
-            echo '<a class="sinSub" href="/usuario.php?id='.$busquedaUsuarios[$i]['nick'].'">';
-                echo '<div class="contenidoTarjeta">';
-                    echo '<p>' . $busquedaUsuarios[$i]['nick'] . '</p>';
-                 //   echo '<p>' . $busquedaUsuarios[$i]['Nombre'] . '</p>';
-                  //  echo '<p>' . $busquedaUsuarios[$i]['Apellidos'] . '</p>';
-                echo '</div>';
-            echo '</a>';
-        echo '</div>';
-        }
-    ?>
-    </div>
-
-    <div class="tarjetas" id="articulos" >
-    <?php
-        for ($i = 0; $i < sizeof($busquedaArticulos) ; $i++){
-            echo '<div class="tarjeta">';
-              
-                    echo '<div class="infoTarjeta" style="background-image: url(&quot;/files/img/articulos/' . $busquedaArticulos[$i]['img']. '&quot;);">';
-                    echo '<a class="sinSub" href="/articulo.php?id='.$busquedaArticulos[$i]['id'].'">';
-                    ?>
-                    <img class="imgUser" src="/files/img/rol/<?php echo $busquedaArticulos[$i]['rol'].'.jpg';?>">
-                    <?php
-                       echo '</a>'; 
-                        echo  '<p> ' . $busquedaArticulos[$i]['nivel']. '</p>';
-                       // echo  '<p> ' . $busquedaArticulos[$i]['capitulo']. '</p>';
-                        //echo  '<p> ' . $busquedaArticulos[$i]['Imagen'] . '</p>';
-                        echo '<a class="sinSub" href="/articulo.php?id='.$busquedaArticulos[$i]['id'].'">';
-                       ?>
-                      
-                      <svg class="SVGsecundario iconoTarjeta" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                      viewBox="0 0 294.843 294.843" style="enable-background:new 0 0 294.843 294.843;" xml:space="preserve">
-                 <g>
-                     <path d="M278.527,79.946c-10.324-20.023-25.38-37.704-43.538-51.132c-2.665-1.97-6.421-1.407-8.392,1.257s-1.407,6.421,1.257,8.392
-                         c16.687,12.34,30.521,28.586,40.008,46.983c9.94,19.277,14.98,40.128,14.98,61.976c0,74.671-60.75,135.421-135.421,135.421
-                         S12,222.093,12,147.421S72.75,12,147.421,12c3.313,0,6-2.687,6-6s-2.687-6-6-6C66.133,0,0,66.133,0,147.421
-                         s66.133,147.421,147.421,147.421s147.421-66.133,147.421-147.421C294.842,123.977,289.201,100.645,278.527,79.946z"/>
-                     <path d="M109.699,78.969c-1.876,1.067-3.035,3.059-3.035,5.216v131.674c0,3.314,2.687,6,6,6s6-2.686,6-6V94.74l88.833,52.883
-                         l-65.324,42.087c-2.785,1.795-3.589,5.508-1.794,8.293c1.796,2.786,5.508,3.59,8.294,1.794l73.465-47.333
-                         c1.746-1.125,2.786-3.073,2.749-5.15c-0.037-2.077-1.145-3.987-2.93-5.05L115.733,79.029
-                         C113.877,77.926,111.575,77.902,109.699,78.969z"/>
-                 </g>
-                 </svg>
-                        <?php
-                        echo '</a>';
-                    echo '</div>';
-                    echo '<a class="sinSub" href="/articulo.php?id='.$busquedaArticulos[$i]['id'].'">';
-                    echo '<div class="contenidoTarjeta">';
-                        echo  '<p> ' . $busquedaArticulos[$i]['titulo']. '</p>';
-                        echo  '<p> ' . $busquedaArticulos[$i]['creador']. '</p>';
-                    //    echo  '<p> ' . $busquedaArticulos[$i]['rol_articulo'] . '</p>';
-                    // echo  '<p> ' . $busquedaArticulos[$i]['contenido'] . '</p>';
-                    echo '</div>';
-                    echo '</a>';
-            echo '</div>';
-        }
-    ?>
-    </div>
-
-    <div class="tarjetas" id="ejercicios" style="display:none">
-    <?php
-        for ($i = 0; $i < sizeof($busquedaEjercicios) ; $i++){
-            echo '<div class="ejercicio">';
-                echo  '<p>' . $busquedaEjercicios[$i]['enunciado'] . '</p>';
-            echo '</div>';
-        }
-    ?>
-    </div>
-
-    <?php
-        if (isset($_SESSION["login"])){
-    ?>
-    <?php
+    ?>       
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" text="ROL" style="height: 225px; width: 100%; display: block;" src="/files/img/rol/<?php echo $busquedaUsuarios[$i]['rol'].'.jpg';?>" data-holder-rendered="true">
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer..</p>
+                        
+                        <div class="botonera">   
+                            <div class="col-sm-6">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                            </div>
+                            <div class="col-sm-6" id="botonera">
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            <?php
        }
     ?>
+        </div>
+    </div>
 </div>
+
