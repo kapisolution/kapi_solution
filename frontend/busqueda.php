89fbbe8bc6a3a072857571fd5f2a45d1f933c69d
alvasenj <alvasenj@ucm.es>
2018-02-17 14:21:04 +0100

    dando formato bussqueda usuarios

--- frontend/busqueda.php
+++ frontend/busqueda.php
@@ -2,37 +2,37 @@
      require 'backend/busqueda.php';
 ?>
 <div class="content">
-<div class="album py-5 bg-light">
-    <h3> Resultados de: <?php echo $_GET["search"] ?> </h3>
-    <div class="container">
-         <div class="row">
-    <?php
-        for ($i = 0; $i < sizeof($busquedaUsuarios) ; $i++){
-    ?>       
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <img class="card-img-top" text="ROL" style="height: 225px; width: 100%; display: block;" src="/files/img/rol/<?php echo $busquedaUsuarios[$i]['rol'].'.jpg';?>" data-holder-rendered="true">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer..</p>
-                        
-                        <div class="botonera">   
-                            <div class="col-sm-6">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+    <div class="album py-5 bg-light">
+        <h3><center>Resultados de: <?php echo $_GET["search"] ?></center> </h3>
+        <br>
+        <div class="container">
+            <div class="row">
+        <?php
+            for ($i = 0; $i < sizeof($busquedaUsuarios) ; $i++){
+        ?>       
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" text="NICK" style="height: 150px; width: 100%; display: block;" src="/files/img/usuario/<?php echo $busquedaUsuarios[$i]['nick'].'.jpg';?>" data-holder-rendered="true">
+                            <div class="card-body">
+                                <ul class="list-unstyled mt-3 mb-4">
+                                    <p><center><?php echo $busquedaUsuarios[$i]['nick']?></center></p>
+                                    <p><center><?php echo $busquedaUsuarios[$i]['nivel']?></center></p>
+                                </ul>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Visitar</button>
+                                    </div>
+                                    <img class="card-img-bottom" text="ROL" style="height: 50px; width: 50px; display: block;" src="/files/img/rol/<?php echo $busquedaUsuarios[$i]['rol'].'.jpg';?>" data-holder-rendered="true">
                                 </div>
                             </div>
-                            <div class="col-sm-6" id="botonera">
-                                <small class="text-muted">9 mins</small>
-                            </div>
-                        </div>
                         </div>
                     </div>
-                </div>
-            <?php
-       }
-    ?>
+                <?php
+        }
+        ?>
+            </div>
         </div>
     </div>
 </div>
+
 
