5c64320d4c330adafd01dbeb8203ff6083894d46
alvasenj <alvasenj@ucm.es>
2018-02-18 17:58:45 +0100

    tarjetas busqueda articulo completado

--- frontend/busqueda.php
+++ frontend/busqueda.php
@@ -9,6 +9,7 @@
         <button type="button" class="btn btn-primary" onclick="mostrarTarjetas(event, 'articulos')">Artículos</button>
         <button type="button" class="btn btn-primary" onclick="mostrarTarjetas(event, 'ejercicios')">Ejercicios</button>
     </div>
+    <br>
     <!--BusquedaUsuarios-->
     <div class="album py-5 bg-light" id="usuarios"> 
         <div class="container" >
@@ -26,13 +27,15 @@
                         <h4 class="media-heading"><?php echo $busquedaUsuarios[$i]['nick']?> <small><i><?php echo $busquedaUsuarios[$i]['email']?></i></small></h4>
                         <p>Nivel: <?php echo $busquedaUsuarios[$i]['nivel']?></p>
                     </div>
-                    <div class="media-right media-top">
-                        <img id="imgRol" src="/files/img/rol/<?php echo $busquedaUsuarios[$i]['rol'].'.jpg'?>" class="media-object">
+                    <div class="media-right">
+                        <div class="media-top">
+                            <center><img id="imgRol" src="/files/img/rol/<?php echo $busquedaUsuarios[$i]['rol'].'.jpg'?>" class="media-object"></center>
+                        </div>
+                        <br>
+                        <div class="media-bottom">
+                            <button type="button" class="btn btn-primary" href=#>Ver Perfil</button>
+                        </div>
                     </div>
-                    <div class="media-right media-bottom">
-                        <button type="button" class="btn btn-primary" href=#>Ver Perfil</button>
-                    </div>
-                    
                 </div>
                 <hr>
         <?php
@@ -53,16 +56,21 @@
                         <div class="card mb-4 box-shadow">
                             <img class="card-img-top" text="NICK" style="height: 150px; width: 100%; display: block;" src="/files/img/articulos/<?php echo $busquedaArticulos[$i]['img'].'.jpg';?>" data-holder-rendered="true">
                             <div class="card-body">
-                                <ul class="list-unstyled mt-3 mb-4">
-                                    <p><center><?php echo $busquedaArticulos[$i]['titulo']?></center></p>
-                                    <p><center><?php echo $busquedaArticulos[$i]['creador']?></center></p>
-                                </ul>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Visitar</button>
+                                <div class="card-block">
+                                    <h4 class="card title"><titulo><?php echo $busquedaArticulos[$i]['titulo']?><titulo></center></h4>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item" style="background-image: url(/files/img/rol/<?php echo $busquedaArticulos[$i]['rol'].'.jpg';?>);background-size:100% 200%;background-repeat:no-repeat;text-transform: uppercase;"><rol><center><?php echo $busquedaArticulos[$i]['rol']?></center></rol></li>
+                                        <li class="list-group-item"><center>Nivel: <?php echo $busquedaArticulos[$i]['nivel']?><center></li>  
+                                    </ul>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                        </div>
+                                        <img id="imgUsr" src="/files/img/usuario/<?php echo $busquedaArticulos[$i]['creador'].'.jpg';?>" class="media-object">
                                     </div>
-                                    <img class="card-img-bottom" text="ROL" style="height: 50px; width: 50px; display: block;" src="/files/img/rol/<?php echo $busquedaArticulos[$i]['rol'].'.jpg';?>" data-holder-rendered="true">
                                 </div>
+                                
                             </div>
                         </div>
                     </div>
