d97f3931105c40a3db9e46aa4eaeaa757a12fab1
Javier Martín <javmar12@ucm.es>
2018-02-22 19:20:18 +0100

    Consulta a la base de datos por las preguntas asociadas a un artículo relacionadas por su id
    Devuelve un hasmap con los datos del articulo
    Un array de preguntas con sus atibutos
    y el numero de preguntas del articulo para
    pintarlas (n_preguntas)

--- backend/articulo.php
+++ backend/articulo.php
@@ -4,7 +4,7 @@ if(isset($_GET["id"])&&$_GET["id"]!=""){
     $consulta = mysqli_query($con, $sql) or die("Error en consulta sobre la tabla articulos");
     $articulo = mysqli_fetch_array($consulta);
 
-  /*  $sql ='SELECT * FROM Preguntas WHERE id = '.$_GET["id"] .';';
+    $sql ='SELECT * FROM Preguntas WHERE id = '.$_GET["id"] .';';
     $consulta = mysqli_query($con, $sql) or die("Error en consulta sobre la tabla articulos");
     $preguntas = array();
     $n_preguntas=0;
@@ -12,7 +12,5 @@ if(isset($_GET["id"])&&$_GET["id"]!=""){
         $preguntas[] = $pregunta;
         $n_preguntas++;
     }
-
-    echo $preguntas[0]['id'];*/
 }
 ?>
