84f077c7f093359fda19a1ae56e439de41d6d536
alvasenj <alvasenj@ucm.es>
2018-03-01 20:57:09 +0100

    modificada topbar

--- frontend/topbar.php
+++ frontend/topbar.php
@@ -1,5 +1,4 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
-<a class="navbar-brand" href="/"><img src="/files/icon/logo.png" alt="kapi solution"></a>
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -7,12 +6,12 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
+      <a class="navbar-brand" href="/"><img src="/files/icon/logo.png" alt="kapi solution"></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-        <div class="contenidoNavbar">
         <ul class="nav navbar-nav">
-        <li>
-            <form class="form-wrapper cf" action="busqueda.php">
+            <li>
+                <form class="form-wrapper cf" action="busqueda.php">
                 <input class="" name="search" type="text" aria-label="Search" 
                     placeholder=	
                         <?php 
@@ -20,42 +19,56 @@
                         ?>
                 >
                 <button class="" type="submit">BUSCAR</button>
-            </form>
-        </li>
-        <?php
-        if(isset($_SESSION["login"])){
+                </form>
+            </li>
+            <?php
+            if(isset($_SESSION["login"])){
+                ?>
+            <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">COMUNIDAD<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Añadir contenido</a></li>
+                <li class="divider"></li>
+                <li><a "dropdown-item" href="#">Modificar contenido</a></li>
+            </ul>
+            </li>
+            <?php
+            }
             ?>
-        <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">COMUNIDAD<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-            <li><a href="#">Añadir contenido</a></li>
-            <li><a href="#">Modificar contenido</a></li>
-        </ul>
-        </li>
-        <?php
-        }
-        ?>
-        <li><a href="#">ARTÍCULOS</a></li>
-        <li><a href="#">EXÁMENES</a></li>
+            <li><a href="#">ARTÍCULOS</a></li>
+            <li><a href="#">EXÁMENES</a></li>
         </ul>
         <?php
-        if(isset($_SESSION["login"])){
+            if(isset($_SESSION["login"])){
+            ?>  
+                <ul class="nav navbar-nav navbar-right" id="login">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><img class="img-rounded" id="imgUsr" src="/files/img/usuario/<?php echo $_SESSION["nick"]?>.jpg"></img><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item">Registrado como <?php echo $_SESSION["nick"]?></a></li>
+                        <li class="divider"></li>
+                        <li><a class="dropdown-item" href="#">Ver Perfil</a></li>
+                        <li class="divider"></li>
+                        <li><a class="dropdown-item" href="backend/logout.php">Cerrar Sesion</a></li>
+                    </ul> 
+                </li>
+                </ul>
+            <?php
+            }else{
+            ?>
+                <ul class="nav navbar-nav navbar-right" id="login">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown"><span class="label mylabel">SignIn</span><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/login.php">Inicia sesión</a></li>
+                        <li class="divider"></li>
+                        <li><a class="dropdown-item" href="/signin.php">Regístrate</a></li>
+                    </ul> 
+                </li>
+                </ul>
+            <?php
+            }
         ?>
-        <ul class="nav navbar-nav navbar-right" id="login">
-        <?php echo '<li><a href="#"><span class="glyphicon glyphicon-user"></span> Bienvenido ' .$_SESSION["nick"]. '</a></li>'?>
-        <li><a href="/backend/logout.php"><span class="glyphicon glyphicon-log-in"></span> CERRAR SESIÓN</a></li>
-        </ul>
-        <?php
-        }else{
-        ?>
-        <ul class="nav navbar-nav navbar-right" id="login">
-        <li><a href="/signin.php"><span class="glyphicon glyphicon-user"></span> Regístrate</a></li>
-        <li><a href="/login.php"><span class="glyphicon glyphicon-log-in"></span> INICIAR SESIÓN</a></li>
-        </ul>
-        <?php
-        }
-        ?>
-    </div>
     </div>
   </div>
 </nav>
