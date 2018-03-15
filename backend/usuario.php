be08e91ecd4bf388c02fadfacb27096b8324806c
Javier Martín <javmar12@ucm.es>
2018-03-15 19:01:09 +0100

    Al cargar la pagina de usuario se cargan las contribuciones de este:
    - $contribucionesUsuarioNoPublicadas[] : para las contribuciones del
    usuario que están en revisión
    
    - $contribucionesUsuario : para las contribuciones del usuario que
    ya han pasado la fase de revisión.

--- backend/usuario.php
+++ backend/usuario.php
@@ -3,5 +3,20 @@ if(isset($_GET["id"])&&$_GET["id"]!=""){
     $sql ='SELECT * FROM Usuarios WHERE nick = "'. $_GET["id"] .'";';
     $consulta = mysqli_query($con, $sql) or die("Error en consulta sobre la tabla Usuarios");
     $usuario = mysqli_fetch_array($consulta);
+
+    $sql ='SELECT * FROM Modificacion_articulo WHERE creador = "'. $_GET["id"] .'" ORDER BY fecha DESC;';
+    $consulta = mysqli_query($con, $sql) or die("Error en consulta sobre la tabla contribuciones usuario");
+    $contribucionesUsuarioNoPublicadas = array();
+    while($fila = mysqli_fetch_array($consulta)){
+        $contribucionesUsuarioNoPublicadas[] = $fila;
+    }
+    
+    $sql ='SELECT * FROM Articulos WHERE creador = "'. $_GET["id"] .'" ORDER BY fecha DESC;';
+    $consulta = mysqli_query($con, $sql) or die("Error en consulta sobre la tabla articulos de usuario");
+    $contribucionesUsuario = array();
+    while($fila = mysqli_fetch_array($consulta)){
+        $contribucionesUsuario[] = $fila;
+    }
+
 }
 ?>
