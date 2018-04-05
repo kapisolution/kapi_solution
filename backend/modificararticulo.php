202ab0dbdd899a355fc69f45306f05774a47993a
Javi <javimv36@gmail.com>
2018-04-05 17:49:20 +0200

    Subiendo articulo con entidad relacionada

--- backend/modificararticulo.php
+++ backend/modificararticulo.php
@@ -9,9 +9,21 @@
             $contenido=$_POST["contenido"];
             $creador= $_SESSION['nick'];
             $id=$_POST["id"];
+            $pregunta1=$_POST["pregunta1"];
+            $pregunta2=$_POST["pregunta2"];
+            $pregunta3=$_POST["pregunta3"];
             echo $id.$rol.$nivel.$titulo.$contenido.$creador;
             $sql = "INSERT INTO Modificacion_articulo (id, rol, nivel, titulo, contenido, creador) VALUES ('$id', '$rol', '$nivel', '$titulo', '$contenido', '$creador');";
             $consulta = mysqli_query($con, $sql) or die("No se pudo insertar el articulo");    
+            
+            $id_mod = mysqli_insert_id($con);
+            $sql = "INSERT INTO Modificacion_pregunta (rol, id_mod, nivel, texto, A, B, C, D, Correcta, id) VALUES ('$rol', '$id_mod', '$nivel', '$pregunta1', 'A', 'B', 'C', 'D', 'A', '$id');";
+            $consulta = mysqli_query($con, $sql) or die("No se pudo insertar la pregunta"); 
+            $sql = "INSERT INTO Modificacion_pregunta (rol, id_mod, nivel, texto, A, B, C, D, Correcta, id) VALUES ('$rol', '$id_mod', '$nivel', '$pregunta2', 'A', 'B', 'C', 'D', 'A', '$id');";
+            $consulta = mysqli_query($con, $sql) or die("No se pudo insertar la pregunta"); 
+            $sql = "INSERT INTO Modificacion_pregunta (rol, id_mod, nivel, texto, A, B, C, D, Correcta, id) VALUES ('$rol', '$id_mod', '$nivel', '$pregunta3', 'A', 'B', 'C', 'D', 'A', '$id');";
+            $consulta = mysqli_query($con, $sql) or die("No se pudo insertar la pregunta"); 
+            
         }
     }
     require("desconexion.php");
