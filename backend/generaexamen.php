913713f0b0f3865b57b4d88b9d915bd8a958640a
Javi <javimv36@gmail.com>
2018-05-02 23:08:54 +0200

    consolidando variables de generacion de examen

--- backend/generaexamen.php
+++ backend/generaexamen.php
@@ -1,7 +1,8 @@
 <?php
     if(isset($_SESSION['login'])){
-        $nivel=$_GET["nivel"];
-        $rol=$_GET["rol"];
+        $nivel=$_SESSION["nivel"];
+        $rol=$_SESSION["rol"];
+        $nivel++;
         $sql ='SELECT * FROM Preguntas WHERE nivel = '.$nivel.' AND rol = "'.$rol.'" ORDER BY RAND() LIMIT 10;';
         $consulta = mysqli_query($con, $sql) or die("Error en consulta sobre la tabla articulos");
         $examen = array();
